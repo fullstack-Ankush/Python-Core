fb0185137d662eeec0cac4f01e43467e702334fc
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-07-21 14:43:51 +0530

    Assignment 3 completed

---
+++ python/Assignment 3/solutions.py
@@ -0,0 +1,15 @@
+# solution 1 
+print("MySirG")
+
+#solution 2
+print("MySirG")
+print("Education Services")
+
+#solution 3
+print('"MySirG"')
+
+#solution 4
+print('"Happy Teacher\'s Days"')
+
+#solution 5
+print("\n")
