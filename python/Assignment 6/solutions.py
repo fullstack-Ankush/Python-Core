4e661a9aa8aa2238cb713f9fa446f859a7e8599d
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-07-21 22:14:37 +0530

    Assignment 6 completed

---
+++ python/Assignment 6/solutions.py
@@ -0,0 +1,25 @@
+#solution 1 
+a = 5
+print(type(a))
+a = str(a)
+print(type(a))
+
+#solution 2
+x = 'm'
+x = ord(x)
+print(x)
+
+#solution 3 
+y = 100
+y = chr(100)
+print(y)
+
+#solution 4
+p = "5"
+p = int(p)
+print(p)
+
+#solution 5
+q = 0
+q = bool(q)
+print(q)
