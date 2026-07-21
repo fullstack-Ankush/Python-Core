879f94b48c1e128c41e474e8d1f716432e69e0e9
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-07-21 14:51:58 +0530

    assignment 4 completed

---
+++ python/Assignment 4/solutions.py
@@ -0,0 +1,32 @@
+# solution 1
+x = 5
+print(x)
+
+# solution 2
+name = 'Ankush'
+print(name)
+
+#solution 3
+y = 3
+z = 2
+print(x)
+print(y)
+print(z)
+
+#solution 4
+a = 35
+b = "proxor"
+c= True
+d = 3+3j
+e = 23.14
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print(d,type(d))
+print(e,type(e))
+
+#solution 5
+date = 21 
+month = "Jul"
+year = 2026
+print(date,month,year,sep="/")
