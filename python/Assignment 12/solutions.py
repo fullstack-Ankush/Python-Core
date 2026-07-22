73ff69bad8e71e4d18b7ee4b2628e34d9be9dd38
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-07-22 20:25:40 +0530

    Assignment 12 initated and completed

---
+++ python/Assignment 12/solutions.py
@@ -0,0 +1,47 @@
+#solution 1 
+x = int(input("Enter a number "))
+if x>0:
+    print("Positive number ")
+if x <0:
+    print("Non-Positive number ")
+
+
+
+
+
+# solution 2
+y = int(input("Enter a number "))
+if y % 5 == 0:
+    print("Divisible by 5")
+else : 
+    print("Non divisible by 5")
+
+# solution 3
+z = int(input("Enter a number "))
+
+if z%2==0:
+    print("even number")
+else:
+    print("odd number")
+
+
+
+
+#solution 4
+a = 4
+b = 2
+if(a<b):
+    print(b)
+elif(a>b):
+    print(a)
+else:
+    print(a)
+
+
+#solution 5
+a = "abcd"
+b = "aabc"
+if(a>b):
+    print(a,b)
+else:
+    print(b,a)
