f78eea37ba16dac7b28e6017d0ea667c560cb595
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-07-22 09:13:42 +0530

    Assignment 9 completed

---
+++ python/Assignment 9/solutions.py
@@ -0,0 +1,24 @@
+#solution 1
+p,r,t = 1000,10,2
+si = (p*r*t)/100
+
+#solution 2
+l,b = 29,12
+area = l*b
+
+# #solution 3
+# a = int(input("Enter number 1 "))
+# b = int(input("Enter number 1 "))
+# c = int(input("Enter number 1 "))
+
+# d = (a+b+c)/3
+# print(d)
+
+#solution 4
+l,b,h = 10,20,30
+volume = l*b*h
+
+#solution 5
+x ,y = int(input("Enter number 1 ")),int(input("Enter number 2 "))
+
+print(x*y)
