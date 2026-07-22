ccafc2fe2cb4ba3a59d69bdda952239e4c89851f
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-07-22 09:36:04 +0530

    Assignment 11 completed

---
+++ python/Assignment 11/solutions.py
@@ -0,0 +1,19 @@
+# #solution 1
+# text = "myheroacademia"
+# print("my" in text)
+
+# #solution 2
+# x = input("Enter strings 1 ")
+# y = input("Enter strings 2 ")
+
+# print(x == y)
+
+
+#solution 3
+print(5>10<5) #output : True but this is wrong
+
+#solution 4
+print("Red" and 'White')
+
+#solution 5
+print(True or False)
