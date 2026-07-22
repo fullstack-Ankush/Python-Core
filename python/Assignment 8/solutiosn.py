6fc2878e8be461815793a39c7a383e5fa97bbc1c
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-07-22 08:28:25 +0530

    Assignment 8 completed

---
+++ python/Assignment 8/solutiosn.py
@@ -0,0 +1,21 @@
+# #solution 1
+# a = input("Enter you name ")
+# print(" %s , good morning"%(a))
+
+# #solution 2
+# x = int (input("enter first value"))
+# y = int(input('enter second value'))
+# print("Sum is %d "%(x+y))
+
+
+# #solution 3
+# rad = int(input("enter the radius "))
+# print("Area of circle of radius %d is %f"%(rad,2*3.14*rad))
+
+# #solution 4
+# num = int(input("Enter the value "))
+# print("Sqaure of %d is %d"%(num,num*num))
+
+#solution 5
+char = input("Enter the character")
+print("the unicode of %s is %d"%(char,ord(char)))
