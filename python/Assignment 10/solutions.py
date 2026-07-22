dbe8be721cf0828e32cf94be297a3a7c0fcbd063
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-07-22 09:22:45 +0530

    Assignment 10 completed

---
+++ python/Assignment 10/solutions.py
@@ -0,0 +1,26 @@
+#solution 1
+a = 1234
+b = 1234//10
+print(b)
+
+#solution 2
+x = 9834
+b = x%10
+print(b)
+
+#solution 3 
+a,b =2,3
+c = a
+a = b
+b = c
+print(a,b)
+
+#solution 4
+p = int(input("Enter the 3-digit number "))
+result = p//100
+print(result)
+
+#solution 5
+p = int(input("Enter the 3-digit number "))
+result = p//10
+print(result)
