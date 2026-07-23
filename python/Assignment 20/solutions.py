d1f6a5aa53ac99b176af2fa2f30d00d2bac2ff35
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-07-23 22:52:46 +0530

    Assignment 20 completed

---
+++ python/Assignment 20/solutions.py
@@ -0,0 +1,21 @@
+#solution 1 
+
+for i in range(1,11):
+    print(i*5)
+
+# solution 2
+n = int(input("Enter you number "))
+
+for i in range(1,11):
+    print(i*n)
+
+#solution 3
+m = int(input("Enter for m number of multiples"))
+n = int(input("Enter the n number "))
+
+for i in range(1,m+1):
+    print(i*n)
+
+#solution 4
+for i in range(10,0,-1):
+    print(i*5)
