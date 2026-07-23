cd0639f931f1a201b9d5b9ca0bb0997d09d72eab
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-07-23 20:33:54 +0530

    Assignment 16 initated and completed

---
+++ python/Assignment 16/solutions.py
@@ -0,0 +1,25 @@
+# solution 1
+a = 1
+while(a<=20):
+    if(a%2 == 0):
+        print("%d is even"%(a))
+        a += 1
+
+#solution 2
+b = 20
+while(b>=20):
+    print("%d is even"%(b))
+    b -= 1
+
+#solution 3
+
+c = 1
+while(c<=10):
+    print(c*c)
+    c += 1
+
+d = 1
+while(d<=10):
+    print(c**3)
+    c += 1
+
