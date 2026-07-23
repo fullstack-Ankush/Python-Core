3b0db3c16e7afe8747736901b7430df0f049bdaa
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-07-23 19:55:41 +0530

    Assignment 15 initiated and complete

---
+++ python/Assignment 15/solutions.py
@@ -0,0 +1,37 @@
+# solution 1
+
+i = 1
+while (i < 6):
+    print("MysirG")
+    i += 1
+j= 1
+#solution 2
+
+
+while(j<11):
+    print(j)
+    j += 1
+
+#solution 3
+
+k = 10
+while( k>=1):
+    print(k)
+    k -= 1
+
+# solution 4
+
+a = 1
+
+while(a <= 20):
+    if(a%2 != 0):
+        print("%d is odd "%(a))
+    a += 1
+
+#solution 5
+
+b = 20
+while(b>= 1):
+    if(b%2 == 1):
+        print("%d is odd "%(b))
+    b -= 1
