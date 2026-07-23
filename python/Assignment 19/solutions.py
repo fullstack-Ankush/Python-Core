d4d8d440c8a6882f58825c6c55b657c8cd8ce1d2
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-07-23 22:39:52 +0530

    Assignemnt 19 finish except solution 4

---
+++ python/Assignment 19/solutions.py
@@ -0,0 +1,26 @@
+# #solution 1 
+# user = input("Enter your string ")
+# # for i in user:
+# #     print("unicode of %c is %i"%(i,ord(i)))
+
+# # solution 2
+# vowels = 0
+# for i in user :
+#     if(i =='a' or i == 'e' or i =='i' or i == 'o' or i =='u'):
+#         vowels += 1
+# print(vowels)
+
+# #solution 3
+# spaceCount = 0
+# for i in user:
+#     if i == " ":
+#         spaceCount += 1
+# print(spaceCount)
+
+#solution 4
+index = 0
+n = input("Enter your number")
+for i in  range(1,len(n)):
+    if(n[i-1] == n[i]):
+        continue
+    print(n[i],end="")
--- python/Assingment 17/solutions.py
+++ python/Assingment 17/solutions.py
@@ -10,3 +10,20 @@ while(i<=n):
     print(i)
     i += 1
 
+#solution 3
+while(n >= 1):
+    print(n)
+    n -= 1
+
+#solution 4
+j = 1
+while(j<=n):
+    if(j%2 == 1):
+        print("%d is Odd"%(j))
+    j += 1
+
+#solution 5
+while(n>=1):
+    if(n%2 == 1):
+        print("%d is Odd"%(n))
+
