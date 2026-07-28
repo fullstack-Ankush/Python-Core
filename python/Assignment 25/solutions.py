f168cd27cb301ee2612b28e8c8b23e91937f9f07
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-07-28 20:22:54 +0530

    Assignment 25 updated

---
+++ python/Assignment 25/solutions.py
@@ -0,0 +1,39 @@
+# # solution 1
+
+# n = int(input("Enter a number "))
+# l1 = []
+# if(n>0):
+#     for i in range(1,n+1):
+#         l1.append(i)
+# print(l1)
+
+# # solution 2
+# l2=[]
+# fterm = 0
+# secondterm = 1
+
+# for i in range(n):
+#     nexterm = fterm + secondterm
+#     l2.append(nexterm)
+#     fterm = secondterm
+#     secondterm = nexterm
+
+# print(l2)
+
+# # solution 3
+# l3 =[]
+# for i in range(2,n+1):
+#     for j in range(2,i):
+#         if i%j == 0:
+#             break
+#     else:
+#         l3.append(i)
+
+# print(l3)
+
+# solution 4
+
+matrix1 =[[1,2,3],[4,5,6],[7,8,9]]
+matrix2 =[[1,2,3],[4,5,6],[7,8,9]]
+
+print(bbbbbbbnnnnnn)
