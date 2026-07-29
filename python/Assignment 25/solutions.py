df3e1ad4c135db2ea38052d6db52cec2bd143824
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-07-29 21:49:11 +0530

    Assignment 25 completed

--- python/Assignment 25/solutions.py
+++ python/Assignment 25/solutions.py
@@ -33,7 +33,29 @@
 
 # solution 4
 
-matrix1 =[[1,2,3],[4,5,6],[7,8,9]]
-matrix2 =[[1,2,3],[4,5,6],[7,8,9]]
+# matrix1 =[[1,2,3],[4,5,6],[7,8,9]]
+# matrix2 =[[1,2,3],[4,5,6],[7,8,9]]
 
-print(bbbbbbbnnnnnn)
+# result =[[0,0,0],[0,0,0],[0,0,0]]
+
+# for i in range(0,3):
+#     for j in range(0,3):
+#         result[i][j] = matrix1[i][j] + matrix2[i][j]
+#         print(result[i][j],end=" ")
+#     print()
+
+# print()
+
+# solution 5
+list1=[1,2,3,-2,-1]
+
+positive = []
+negative = []
+
+for i in list1:
+    if i<0:
+        negative.append(i)
+    else:
+        positive.append(i)
+print(negative)
+print(positive)
