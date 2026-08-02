8d9af4dc00f913787cffd965412975d3c92f73d0
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-08-02 18:21:38 +0530

    Assignment 32 completed

---
+++ python/Assignment 32/solutions.py
@@ -0,0 +1,25 @@
+# solution 1
+
+def sum(a,b):
+    return a + b
+
+result1 = sum(10,20)
+print(result1)
+
+# solution 2
+def areaCircle(r):
+    return 3.14 * r * r
+
+# solution 3
+
+def average3(a,b,c):
+    return (a + b + c) / 3
+
+# solution 4 
+def compoundInterest(p,r,t):
+    return p * (1+r/100)**t
+
+# solution 5
+def volumeCuboid(l,b,h):
+    return l * b * h
+
